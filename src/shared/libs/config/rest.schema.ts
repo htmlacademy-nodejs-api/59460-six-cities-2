c3d69dbc87c330595dd1c014d67c8fcd108d985e
websh <shamrilods@yandex.ru>
2026-01-09 19:43:56 +0300

    add MongoDB support with Mongoose, environment variables for DB configuration, and Docker setup

--- src/shared/libs/config/rest.schema.ts
+++ src/shared/libs/config/rest.schema.ts
@@ -4,6 +4,10 @@ export const configRestSchema = z.object({
   PORT: z.coerce.number().default(4000),
   SALT: z.string(),
   DB_HOST: z.ipv4().default('127.0.0.1'),
+  DB_USER: z.string(),
+  DB_PASSWORD: z.string(),
+  DB_PORT: z.coerce.number().default(27017),
+  DB_NAME: z.string(),
 });
 
 export type RestSchema = z.infer<typeof configRestSchema>;
